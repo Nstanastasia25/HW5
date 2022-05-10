73561c737fa12e500f3d1048601cce7c4bc2e1db
Anastasia <nstanastasia25@yandex.ru>
2022-05-10 17:52:31 +0300

    added ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int[] numberA(int count) //-Создание массива A
+{
+    int[] result = new int[count];
+    return result;
+}
+int[] numberB(int count) //-Создание массива B
+{
+    int[] result = new int[count];
+    return result;
+}
